e4b8903fa974d5e79b871e1dd08c9d47e01ab296
Bogdan Gevko <bgevko@gmail.com>
2024-05-17 11:03:57 +0300

    Added eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,83 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "plugin:import/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ["react", "react-hooks", "react-refresh", "import"],
+  settings: {
+    react: {
+      reactVersion: "detect",
+    },
+    "import/resolver": {
+      node: true,
+      typescript: true,
+    },
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs", "node_modules", "vite.config.ts"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        pathGroups: [
+          {
+            pattern: "$/**",
+            group: "internal",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        groups: [
+          ["builtin", "external"],
+          ["internal"],
+          ["parent", "sibling", "index"],
+          "unknown",
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "import/no-default-export": "error",
+    "import/no-extraneous-dependencies": "error",
   },
-}
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: ["plugin:import/typescript"],
+    },
+    {
+      files: [
+        // Next pages files
+        "src/pages/**/*.tsx",
+        // Typescript declaration file
+        "additional.d.ts",
+        // Graphql Codegen generated file
+        "**/instrospection.ts",
+      ],
+      rules: {
+        "import/no-anonymous-default-export": "off",
+        "import/no-default-export": "off",
+      },
+    },
+  ],
+};
