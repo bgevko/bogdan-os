1c1a8ecc4c70f49f5a56209886abab0f76410f72
Bogdan Gevko <bgevko@gmail.com>
2024-05-17 14:32:17 +0300

    Added vitest and react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
