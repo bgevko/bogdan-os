bd2ab90c0071719b2e1372b1745b82a490701d12
Bogdan Gevko <bgevko@gmail.com>
2024-05-17 11:04:30 +0300

    Configured import aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import reactLogo from "@/assets/react.svg";
+import viteLogo from "@/public/vite.svg";
+
+import "./App.css";
+
+export function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +31,5 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
-
-export default App
