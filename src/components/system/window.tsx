61c99993f43c0fc439a31cde61944e01194fc23c
Bogdan Gevko <bgevko@gmail.com>
2024-05-19 19:15:33 +0300

    Changed app window placeholder size and position

--- src/components/system/window.tsx
+++ src/components/system/window.tsx
@@ -1,7 +1,9 @@
 import { type ReactElement, ReactNode } from 'react';
 
 const Window = ({ children }: { children: ReactNode }): ReactElement => (
-  <section className="bg-surface text-onSurface">{children}</section>
+  <section className="mx-auto my-96 flex size-40 items-center justify-center bg-surface text-onSurface">
+    {children}
+  </section>
 );
 
 export default Window;
