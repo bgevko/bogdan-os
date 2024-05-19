661ea9761ad953fe14b4e918d5430da886570af8
Bogdan Gevko <bgevko@gmail.com>
2024-05-19 08:46:34 +0300

    Wrapped process loader inside desktop component

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,10 @@
+import Desktop from '@/components/system/desktop';
 import ProcessLoader from '@/components/system/process-loader';
 
-const App = (): React.ReactElement => <ProcessLoader />;
+const App = (): React.ReactElement => (
+  <Desktop>
+    <ProcessLoader />
+  </Desktop>
+);
 
 export default App;
