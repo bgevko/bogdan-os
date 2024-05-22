a8cbe9397d64ef4556286a44e203e786929bc0a8
Bogdan Gevko <bgevko@gmail.com>
2024-05-22 15:56:39 +0300

    Added svgr to support better svg loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vitest/config';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        svgoConfig: {
+          floatPrecision: 2,
+        },
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
