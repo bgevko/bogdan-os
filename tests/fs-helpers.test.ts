61fdfc8c700177b9914277267354e82fdcf2e79b
Bogdan Gevko <bgevko@gmail.com>
2024-06-27 12:56:54 +0300

    Added tests for the new FS system

---
+++ tests/fs-helpers.test.ts
@@ -0,0 +1,86 @@
+import { it, expect, describe } from 'vitest';
+
+import { iconsPath } from '@/constants';
+import { parseFileExt, parseFileName, parseFileInfo, splitPath, parseParentPath } from '@/utils/fs';
+
+const root = '/';
+const path = '/path/to/file.app';
+const folder = '/path/to/folder/';
+const folder2 = '/path/to/folder';
+
+describe('Fs Helpers', () => {
+  it('should return the file name', () => {
+    const fileName = parseFileName(path);
+    const folderName = parseFileName(folder);
+    const folderName2 = parseFileName(folder2);
+    const rootName = parseFileName(root);
+    expect(fileName).toBe('file');
+    expect(folderName).toBe('folder');
+    expect(folderName2).toBe('folder');
+    expect(rootName).toBe('');
+  });
+
+  it('should return the parent path', () => {
+    const parentPath = parseParentPath(path);
+    const parentFolder = parseParentPath(folder);
+    const parentFolder2 = parseParentPath(folder2);
+    const parentRoot = parseParentPath(root);
+    expect(parentPath).toBe('/path/to');
+    expect(parentFolder).toBe('/path/to');
+    expect(parentFolder2).toBe('/path/to');
+    expect(parentRoot).toBe('');
+
+    let test = '/folder/';
+    let res = parseParentPath(test);
+    expect(res).toBe('/');
+    test = '/folder';
+    res = parseParentPath(test);
+    expect(res).toBe('/');
+  });
+
+  it('should return the file extension', () => {
+    const ext = parseFileExt(path);
+    const extFolder = parseFileExt(folder);
+    const extFolder2 = parseFileExt(folder2);
+    const extRoot = parseFileExt(root);
+
+    expect(ext).toBe('app');
+    expect(extFolder).toBe('');
+    expect(extFolder2).toBe('');
+    expect(extRoot).toBe('');
+  });
+
+  it('should return the file info', () => {
+    let res = parseFileInfo(path);
+    expect(res.fileName).toBe('file');
+    expect(res.fileExt).toBe('app');
+    expect(res.icon).toBe(`${iconsPath}/executable.png`);
+
+    res = parseFileInfo('/');
+    expect(res.fileName).toBe('');
+    expect(res.fileExt).toBe('');
+    expect(res.icon).toBe('');
+  });
+
+  it('should correctly split a path into path components', () => {
+    const components = splitPath(path);
+    expect(components).toEqual(['/', 'path', 'to', 'file.app']);
+
+    let test = '/folder/';
+    expect(splitPath(test)).toEqual(['/', 'folder']);
+    test = '/folder';
+    expect(splitPath(test)).toEqual(['/', 'folder']);
+    test = '/';
+    expect(splitPath(test)).toEqual(['/']);
+    test = '';
+    expect(splitPath(test)).toEqual(['/']);
+    test = 'folder';
+    expect(splitPath(test)).toEqual(['/', 'folder']);
+    test = 'folder/';
+    expect(splitPath(test)).toEqual(['/', 'folder']);
+    test = 'folder/file.txt';
+    expect(splitPath(test)).toEqual(['/', 'folder', 'file.txt']);
+    test = '1/2/3/4/5/6/7/8/9/10';
+    expect(splitPath(test)).toEqual(['/', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']);
+  });
+});
--- tests/use-fs.test.tsx
+++ tests/use-fs.test.tsx
@@ -1,76 +1,279 @@
-/* eslint-disable no-relative-import-paths/no-relative-import-paths */
 import { renderHook, act } from '@testing-library/react';
 import { it, expect, describe, beforeEach } from 'vitest';
 
+import { iconsPath } from '@/constants';
 import useFsStore from '@/stores/use-fs-store';
-
-import { testDir } from './globals';
-
-// TODO: Rewrite tests to use the new system
 
 beforeEach(() => {
   const { result } = renderHook(() => useFsStore());
+  const fs = result.current;
   act(() => {
-    result.current.root.loadFrom(testDir);
-    result.current.setCurrentPath('/');
+    expect(fs).toBeDefined();
+    fs.initDir();
+    const paths = [...fs.getPaths()];
+    expect(paths).toEqual(['/']);
   });
 });
 
 describe('useFsStore', () => {
-  it('Should be in the root directory', () => {
-    const { result } = renderHook(() => useFsStore());
+  const { result } = renderHook(() => useFsStore());
+  const fs = result.current;
+  it('should be able to create a directory with mkdir', () => {
     act(() => {
-      expect(result.current.currentPath).toEqual('/');
+      fs.mkdir('/test');
+      const paths = [...fs.getPaths()];
+      expect(paths).toEqual(['/', '/test']);
+      fs.mkdir('/test2');
+      expect([...fs.getPaths()]).toEqual(['/', '/test', '/test2']);
+      fs.mkdir('/test2/');
+      expect([...fs.getPaths()]).toEqual(['/', '/test', '/test2']);
+      fs.mkdir('');
+      expect([...fs.getPaths()]).toEqual(['/', '/test', '/test2']);
+      fs.mkdir('/');
+      expect([...fs.getPaths()]).toEqual(['/', '/test', '/test2']);
     });
   });
-  it('Should change the current path to /Desktop', () => {
-    const { result } = renderHook(() => useFsStore());
+
+  it('should be able to create chain of directories with mkdir', () => {
     act(() => {
-      result.current.setCurrentPath('/Desktop');
-    });
-    act(() => {
-      expect(result.current.currentPath).toEqual('/Desktop');
+      fs.mkdir('/test/test2');
+      const paths = [...fs.getPaths()];
+      expect(paths).toEqual(['/', '/test', '/test/test2']);
+      fs.initDir();
+      fs.mkdir('/1/2/3/4/5');
+      expect([...fs.getPaths()]).toEqual(['/', '/1', '/1/2', '/1/2/3', '/1/2/3/4', '/1/2/3/4/5']);
+      fs.mkdir('/test/test2');
+      expect([...fs.getPaths()]).toEqual([
+        '/',
+        '/1',
+        '/1/2',
+        '/1/2/3',
+        '/1/2/3/4',
+        '/1/2/3/4/5',
+        '/test',
+        '/test/test2',
+      ]);
+      fs.initDir();
+      fs.mkdir('1');
+      expect([...fs.getPaths()]).toEqual(['/', '/1']);
+      fs.initDir();
+      fs.mkdir('1/2/3');
+      expect([...fs.getPaths()]).toEqual(['/', '/1', '/1/2', '/1/2/3']);
+
+      fs.initDir();
+      fs.mkdir('/1/2/');
+      fs.mkdir('1/3/');
+      fs.mkdir('1/4');
+      expect([...fs.getPaths()]).toEqual(['/', '/1', '/1/2', '/1/3', '/1/4']);
+
+      fs.initDir();
+      fs.mkdir('1/hello.txt/3');
+      expect([...fs.getPaths()]).toEqual(['/', '/1', '/1/hello.txt', '/1/hello.txt/3']);
+
+      fs.initDir();
+      fs.mkdir('/1/2/');
+      expect([...fs.getPaths()]).toEqual(['/', '/1', '/1/2']);
     });
   });
-  it('Should throw an error when trying to go to a non-existent directory', () => {
-    const { result } = renderHook(() => useFsStore());
+
+  it('should be able to create a file with touch', () => {
     act(() => {
+      fs.initDir();
+      fs.touch('/test.txt');
+      const paths = [...fs.getPaths()];
+      expect(paths).toEqual(['/', '/test.txt']);
+      fs.touch('/test2.txt');
+      expect([...fs.getPaths()]).toEqual(['/', '/test.txt', '/test2.txt']);
+
+      // should throw error
       expect(() => {
-        result.current.setCurrentPath('/NonExistent');
+        fs.touch('/test3.txt/');
       }).toThrowError();
     });
   });
-  it('Should throw an error when trying to set the current path to a file', () => {
-    const { result } = renderHook(() => useFsStore());
+
+  it('should be able to create chain of directories, leading to the file with touch', () => {
     act(() => {
+      fs.initDir();
+      fs.touch('/test/test2.txt');
+      expect([...fs.getPaths()]).toEqual(['/', '/test', '/test/test2.txt']);
+
       expect(() => {
-        result.current.setCurrentPath('/Desktop/HelloWorld');
+        fs.touch('/test/test2.txt/');
       }).toThrowError();
     });
   });
-  it('Should return children appropriately', () => {
-    const { result } = renderHook(() => useFsStore());
-    act(() => {
-      const rootChildren = result.current.getChildren('/');
-      expect(rootChildren).toEqual(['/Desktop', '/Documents']);
-    });
 
+  it('it should throw an error when trying to touch onto an existing path', () => {
     act(() => {
-      result.current.setCurrentPath('/Desktop');
-      const desktopChildren = result.current.getChildren('/Desktop');
-      expect(desktopChildren).toEqual(['/Desktop/HelloWorld', '/Desktop/MyFolder']);
-    });
+      fs.initDir();
+      fs.mkdir('/test');
+      expect(() => {
+        fs.touch('/test');
+      }).toThrowError();
 
-    act(() => {
-      result.current.setCurrentPath('/');
-      const rootChildren = result.current.getChildren('/Desktop');
-      expect(rootChildren).toEqual(['/Desktop/HelloWorld']);
+      fs.mkdir('/my/nested/test');
+      expect(() => {
+        fs.touch('/my');
+      }).toThrowError();
+      expect(() => {
+        fs.touch('/my/nested');
+      }).toThrowError();
+      expect(() => {
+        fs.touch('/my/nested/test');
+      }).toThrowError();
     });
+  });
 
+  it('should set isDir correctly when using mkdir and touch', () => {
     act(() => {
-      result.current.setCurrentPath('/Documents');
-      const documentsChildren = result.current.getChildren('/Documents');
-      expect(documentsChildren).toEqual([]);
+      fs.initDir();
+      expect(fs.isDir('/')).toBe(true);
+      fs.mkdir('/test');
+      expect(fs.isDir('/test')).toBe(true);
+      fs.mkdir('/test/1/2/3');
+      expect(fs.isDir('/test/1')).toBe(true);
+      expect(fs.isDir('/test/1/2')).toBe(true);
+      expect(fs.isDir('/test/1/2/3')).toBe(true);
+
+      fs.touch('/test.txt');
+      expect(fs.isDir('/test.txt')).toBe(false);
+      fs.touch('/test/1/2/3.txt');
+      expect(fs.isDir('/test')).toBe(true);
+      expect(fs.isDir('/test/1')).toBe(true);
+      expect(fs.isDir('/test/1/2')).toBe(true);
+      expect(fs.isDir('/test/1/2/3.txt')).toBe(false);
+
+      fs.initDir();
+      fs.touch('/1.txt/2/3');
+      expect(fs.isDir('/1.txt')).toBe(true);
+      expect(fs.isDir('/1.txt/2')).toBe(true);
+      expect(fs.isDir('/1.txt/2/3')).toBe(false);
+
+      fs.initDir();
+      fs.mkdir('/1/2/3/');
+      expect(fs.isDir('/1')).toBe(true);
+      expect(fs.isDir('/1/2')).toBe(true);
+      expect(fs.isDir('/1/2/3')).toBe(true);
+    });
+  });
+
+  it('it should set the folder icon correctly', () => {
+    act(() => {
+      fs.initDir();
+      expect(fs.getNode('/').icon).toBe(`${iconsPath}/folder.png`);
+
+      fs.mkdir('/test');
+      expect(fs.getNode('/test').icon).toBe(`${iconsPath}/folder.png`);
+
+      fs.mkdir('/test/1/2/3');
+      expect(fs.getNode('/test/1').icon).toBe(`${iconsPath}/folder.png`);
+      expect(fs.getNode('/test/1/2').icon).toBe(`${iconsPath}/folder.png`);
+      expect(fs.getNode('/test/1/2/3').icon).toBe(`${iconsPath}/folder.png`);
+
+      fs.touch('/test.app');
+      expect(fs.getNode('/test.app').icon).toBe(`${iconsPath}/executable.png`);
+
+      fs.touch('/test/1/2/3.app');
+      expect(fs.getNode('/test').icon).toBe(`${iconsPath}/folder.png`);
+      expect(fs.getNode('/test/1').icon).toBe(`${iconsPath}/folder.png`);
+      expect(fs.getNode('/test/1/2').icon).toBe(`${iconsPath}/folder.png`);
+      expect(fs.getNode('/test/1/2/3.app').icon).toBe(`${iconsPath}/executable.png`);
+
+      fs.initDir();
+      fs.touch('/1.app/2/3');
+      expect(fs.getNode('/1.app').icon).toBe(`${iconsPath}/folder.png`);
+      expect(fs.getNode('/1.app/2').icon).toBe(`${iconsPath}/folder.png`);
+      expect(fs.getNode('/1.app/2/3').icon).toBe(`${iconsPath}/default.png`);
+    });
+  });
+
+  it('should be able to initialize a directory with initDir', () => {
+    const customDir = ['/', '/Desktop/MyFolder/', '/Documents/'];
+    act(() => {
+      fs.initDir(customDir);
+      const paths = [...fs.getPaths()];
+      expect(paths).toEqual(['/', '/Desktop', '/Desktop/MyFolder', '/Documents']);
+      fs.initDir();
+      expect([...fs.getPaths()]).toEqual(['/']);
+    });
+  });
+
+  it('should correctly set files and folders with initDir', () => {
+    act(() => {
+      const customDir = [
+        '/',
+        '/Desktop/',
+        '/Desktop/MyFolder/1',
+        '/Desktop/MyFolder/2',
+        '/Desktop/MyFolder/3',
+        '/Desktop/MyFile',
+        '/Documents/',
+      ];
+
+      fs.initDir(customDir);
+      expect(fs.isDir('/')).toBe(true);
+      expect(fs.isDir('/Desktop')).toBe(true);
+      expect(fs.isDir('/Desktop/MyFolder')).toBe(true);
+      expect(fs.isDir('/Desktop/MyFolder/1')).toBe(false);
+      expect(fs.isDir('/Desktop/MyFolder/2')).toBe(false);
+      expect(fs.isDir('/Desktop/MyFolder/3')).toBe(false);
+      expect(fs.isDir('/Desktop/MyFile')).toBe(false);
+      expect(fs.isDir('/Documents')).toBe(true);
+    });
+  });
+
+  it('should get children nodes correctly', () => {
+    act(() => {
+      const dirSource = [
+        '/',
+        '/Desktop/MyFolder/',
+        '/Desktop/MyFolder/1',
+        '/Desktop/MyFolder/2',
+        '/Desktop/MyFolder/3',
+        '/Desktop/MyFile',
+        '/Documents/',
+      ];
+      fs.initDir(dirSource);
+
+      const rootChildren = [...fs.getChildren('/')];
+      const rootChildrenPaths = rootChildren.map((node) => node.path);
+      expect(rootChildrenPaths).toEqual(['/Desktop', '/Documents']);
+
+      const desktopChildren = [...fs.getChildren('/Desktop')];
+      const desktopChildrenPaths = desktopChildren.map((node) => node.path);
+      expect(desktopChildrenPaths).toEqual(['/Desktop/MyFolder', '/Desktop/MyFile']);
+
+      const myFolderChildren = [...fs.getChildren('/Desktop/MyFolder')];
+      const myFolderChildrenPaths = myFolderChildren.map((node) => node.path);
+      expect(myFolderChildrenPaths).toEqual([
+        '/Desktop/MyFolder/1',
+        '/Desktop/MyFolder/2',
+        '/Desktop/MyFolder/3',
+      ]);
+
+      const myFileChildren = [...fs.getChildren('/Desktop/MyFile')];
+      expect(myFileChildren).toEqual([]);
+    });
+  });
+
+  it('should have working getters and setters for various attributes', () => {
+    act(() => {
+      fs.initDir(['/', '/Desktop/folder/', '/Desktop/file']);
+
+      const folderGridIndex = fs.getGridIndex('/Desktop/folder');
+      expect(folderGridIndex).toBe(0);
+      const fileGridIndex = fs.getGridIndex('/Desktop/file');
+      expect(fileGridIndex).toBe(0);
+      fs.setGridIndex('/Desktop/folder', 1);
+      fs.setGridIndex('/Desktop/file', 2);
+      expect(fs.getGridIndex('/Desktop/folder')).toBe(1);
+      expect(fs.getGridIndex('/Desktop/file')).toBe(2);
+
+      const folderGridItemsPerLine = fs.getGridItemsPerLine('/Desktop/folder');
+      expect(folderGridItemsPerLine).toBe(0);
+      fs.setGridItemsPerLine('/Desktop/folder', 3);
+      expect(fs.getGridItemsPerLine('/Desktop/folder')).toBe(3);
     });
   });
 });
