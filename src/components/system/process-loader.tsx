d717cf854eedb7907cf7e4e29a7d813a7a0a4b92
Bogdan Gevko <bgevko@gmail.com>
2024-06-28 13:59:46 +0300

    Components are no longer part of the processes store, and are loaded separately

--- src/components/system/process-loader.tsx
+++ src/components/system/process-loader.tsx
@@ -1,13 +1,19 @@
 import { Suspense } from 'react';
 
 import Window from '@/components/system/window';
+import { FileExplorer } from '@/constants';
+import useFsStore from '@/stores/use-fs-store';
 import useProcessesStore from '@/stores/use-processes-store';
+import { parseFileComponent } from '@/utils/fs';
 
 const ProcessLoader = (): React.ReactElement => {
   const processes = useProcessesStore((state) => state.openedProcesses);
+  const isDir = useFsStore((state) => state.isDir);
 
   const loading = <div>Loading...</div>;
-  const components = [...processes].map(([key, { Component, hasWindow, path }]) => {
+  const components = [...processes].map(([key, { hasWindow, path }]) => {
+    const Component = isDir(path) ? FileExplorer : parseFileComponent(path);
+
     if (hasWindow) {
       return (
         <Window key={key} path={path}>
