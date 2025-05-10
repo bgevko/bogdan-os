86079fcea7ed8192c83f83cb268d7218ff6c3cf0
Bogdan Gevko <bgevko@gmail.com>
2025-05-10 15:29:52 +0300

    Added input handlers for keyboard and gamepads

--- src/apps/nes-emulator/index.tsx
+++ src/apps/nes-emulator/index.tsx
@@ -1,37 +1,113 @@
-import React, { useEffect, useRef } from 'react';
-
-interface WorkerMessage {
-  type: 'init' | 'frame';
-}
+import { useEffect, useRef, useCallback } from 'react';
+import { InputButtonMessage, InputKeyMessage, WorkerMessage } from './nes.worker';
 
 const NES = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const animationIdRef = useRef<number | null>(null);
   const workerRef = useRef<Worker | null>(null);
+  const keysPressedRef = useRef<Set<string>>(new Set());
 
+  const onGamepadConnect = useCallback((e: GamepadEvent) => {
+    console.log(
+      'Gamepad connected at index %d: %s. %d buttons, %d axes.',
+      e.gamepad.index,
+      e.gamepad.id,
+      e.gamepad.buttons.length,
+      e.gamepad.axes.length,
+    );
+  }, []);
+
+  const onGamepadDisconnect = useCallback((e: GamepadEvent) => {
+    console.log('Gamepad disconnected from index %d: %s', e.gamepad.index, e.gamepad.id);
+  }, []);
+
+  //** Listens for gamepad input */
+  const pollGamepads = useCallback(() => {
+    const gamepads = navigator.getGamepads();
+    if (gamepads.length === 0 || !workerRef.current) {
+      return;
+    }
+
+    for (let gpIdx = 0; gpIdx < gamepads.length; gpIdx++) {
+      const gp = gamepads[gpIdx];
+      if (!gp) continue;
+
+      // Poll which buttons are pressed
+      for (let btnIdx = 0; btnIdx < gp.buttons.length; btnIdx++) {
+        if (gp.buttons[btnIdx].pressed) {
+          const msg: InputButtonMessage = {
+            type: 'input-btn',
+            gp: gpIdx,
+            btn: btnIdx,
+          };
+          workerRef.current.postMessage(msg);
+        }
+      }
+    }
+  }, []);
+
+  //** Sends pressed keys to the nes worker*/
+  const pollKeyboard = useCallback(() => {
+    if (!workerRef.current || keysPressedRef.current.size === 0) return;
+
+    for (const key of keysPressedRef.current) {
+      const msg: InputKeyMessage = {
+        type: 'input-key',
+        key: key,
+      };
+      workerRef.current.postMessage(msg);
+    }
+  }, []);
+
+  //** Any key down key triggers this */
+  const onKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.repeat) return;
+    if (!keysPressedRef.current.has(e.code)) {
+      keysPressedRef.current.add(e.code);
+    }
+  }, []);
+
+  //** Clears key down state */
+  const onKeyUp = useCallback((e: KeyboardEvent) => {
+    if (keysPressedRef.current.has(e.code)) {
+      keysPressedRef.current.delete(e.code);
+    }
+  }, []);
+
+  /*
+################################
+||            Init            ||
+################################
+*/
   useEffect(() => {
     const worker = new Worker(new URL('./nes.worker.js', import.meta.url), { type: 'module' });
     workerRef.current = worker;
 
+    worker.postMessage({ type: 'init' });
     worker.onmessage = (e: MessageEvent<WorkerMessage>) => {
-      if (e.data.type === 'frame') {
-        console.log('Got frame from worker');
-      }
-
       if (e.data.type === 'init') {
-        console.log('Worker is ready');
+        console.log('NES worker ready');
         animationIdRef.current = requestAnimationFrame(frame);
       }
     };
 
-    worker.postMessage({ type: 'init' });
-
+    //** Emulation frame*/
     const frame = () => {
-      worker.postMessage({ type: 'frame' });
+      pollGamepads();
+      pollKeyboard();
       animationIdRef.current = requestAnimationFrame(frame);
     };
 
+    window.addEventListener('gamepadconnected', onGamepadConnect);
+    window.addEventListener('gamepaddisconnected', onGamepadDisconnect);
+    document.addEventListener('keydown', onKeyDown);
+    document.addEventListener('keyup', onKeyUp);
+
     return () => {
+      window.removeEventListener('gamepadconnected', onGamepadConnect);
+      window.removeEventListener('gamepaddisconnected', onGamepadDisconnect);
+      document.removeEventListener('keydown', onKeyDown);
+      document.removeEventListener('keyup', onKeyUp);
       if (animationIdRef.current != null) {
         cancelAnimationFrame(animationIdRef.current);
       }
--- src/apps/nes-emulator/nes.worker.ts
+++ src/apps/nes-emulator/nes.worker.ts
@@ -1,19 +1,53 @@
 // nes.worker.js
 import init, { WasmControlDeck } from '@/wasm/nes/tetanes_core.js';
 
-let deckPromise: Promise<WasmControlDeck> | null = null;
-type WorkerMessage = { type: 'init' } | { type: 'frame' };
+let nes: Promise<WasmControlDeck> | null = null;
+
+export interface InputButtonMessage {
+  type: 'input-btn';
+  gp: number;
+  btn: number;
+}
+
+export interface InputKeyMessage {
+  type: 'input-key';
+  key: string;
+}
+
+export type WorkerMessage =
+  | { type: 'init' }
+  | { type: 'frame' }
+  | InputButtonMessage
+  | InputKeyMessage;
 
 self.onmessage = async (e: MessageEvent) => {
   const { type } = e.data as WorkerMessage;
 
-  if (type === 'init') {
-    deckPromise = init().then(() => new WasmControlDeck());
-    await deckPromise;
-    self.postMessage({ type: 'init' });
-  }
+  switch (type) {
+    case 'init': {
+      nes = init().then(() => new WasmControlDeck());
+      await nes;
+      self.postMessage({ type: 'init' });
+      break;
+    }
 
-  if (type === 'frame' && deckPromise) {
-    self.postMessage({ type: 'frame' });
+    case 'frame': {
+      // self.postMessage({ type: 'frame' });
+      break;
+    }
+
+    case 'input-btn': {
+      const { gp, btn } = e.data as InputButtonMessage;
+      console.log('input-btn:', gp, btn);
+      break;
+    }
+
+    case 'input-key': {
+      const { key } = e.data as InputKeyMessage;
+      console.log('input-key:', key);
+      break;
+    }
+
+    default:
   }
 };
